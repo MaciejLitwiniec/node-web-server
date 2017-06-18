47189f74c1fafee718e89a20d14815c2115e613e
MaciejLitwiniec <litwiniec.maciej@gmail.com>
2017-06-18 14:56:34 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//Partials are reusable... Partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+//.set allows to apply express-related configuration
+app.set('view engine', 'hbs');
+
+//server logger with 'next' as an argument
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) =>{
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+//MAINTENANCE MODE
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+
+//.use - adding middleware
+app.use(express.static(__dirname + '/public'));
+
+//registering Helper
+//first argument is name of the Helper
+//second argument is function that we are passing
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+//main route - as arguments request comming in and response comming out
+app.get('/',(req, res) => {
+  // res.send('<h1>hello express</h1>');
+  // res.send({
+  //   name: 'Maciej',
+  //   likes: [
+  //     'traveling',
+  //     'biking'
+  //   ]
+  // })
+  res.render('home.hbs', {
+      pageTitle: 'Home Page',
+      welcomeMessage: 'Welcome to my page!'
+      //year is now provided by Handlebars Helper
+      //currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+  // res.send('About page');
+  // res.render('about.hbs');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+    //year is now provided by Handlebars Helper
+    //currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad route!'
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
